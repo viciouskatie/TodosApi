d8dc50681c6ceb7f0a23c460996b413305755405
viciouskatie <53979408+viciouskatie@users.noreply.github.com>
2019-09-06 13:36:56 -0500

    corrections

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send(
+app.get('/katharine', (req, res) => res.send(
     {
-      name:'Katharine'
-    }
-))
+      name:'Katharine',
+	  origin: 'female',
+	  children: 2,
+	  parents: 'crazy'
 
+    }
+
+))
+	app.get('/', (req, res) => res.send(
+    'hello world!'
+    ))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
